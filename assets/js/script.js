a2fee089b7b1540a0d6190d542116288bfa4a9ba
lindeluhuang <linde.lu.huang@gmail.com>
2018-10-13 18:06:58 -0400

    added sticky header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('hi');
+console.log('Hello there, you curious person you. Welcome to my site!');
+
+// Sticky header
+const header = document.getElementById("sticky-header");
+const sticky = header.offsetTop;
+header.classList.add("sticky");
