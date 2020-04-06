16711f84d2ab3800ffeefa11a1bb79686157b07d
Linde Huang <lindeh@Lindes-Work-MacBook.local>
2020-04-06 18:34:22 -0400

    adding grids to image outlines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-// window.onload = function() {
-//   document.getElementById("transitionIn").style.marginTop= "4em";
-//   document.getElementById("transitionIn").style.fontSize="3.052em";
-// };
-
-document.addEventListener("DOMContentLoaded", function (){
+window.onload = function() {
   document.getElementById("transitionIn").style.marginTop= "4em";
   document.getElementById("transitionIn").style.fontSize="3.052em";
-}
-);
+};
+
+// document.addEventListener("DOMContentLoaded", function (){
+//   document.getElementById("transitionIn").style.marginTop= "4em";
+//   document.getElementById("transitionIn").style.fontSize="3.052em";
+// }
+// );
