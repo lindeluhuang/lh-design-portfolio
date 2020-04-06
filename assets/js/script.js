79a159c5bc2004b81d1c7f1e1ca21793fa3d9c0b
Linde Huang <lindeh@Lindes-Work-MacBook.local>
2020-04-06 17:21:54 -0400

    more compressing images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-window.onload = function() {
+// window.onload = function() {
+//   document.getElementById("transitionIn").style.marginTop= "4em";
+//   document.getElementById("transitionIn").style.fontSize="3.052em";
+// };
+
+document.addEventListener("DOMContentLoaded", function (){
   document.getElementById("transitionIn").style.marginTop= "4em";
   document.getElementById("transitionIn").style.fontSize="3.052em";
-};
+}
+);
