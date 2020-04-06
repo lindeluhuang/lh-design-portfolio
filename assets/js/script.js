7bae4c0a25f1e122e2e3f031f49b4c600ebbe720
Linde Huang <lindeh@Lindes-Work-MacBook.local>
2020-04-06 11:08:39 -0400

    cleaning up files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,1 @@
 console.log('Hello there, you curious person you. Welcome to my site!');
-
-var navHtml = "<ul class=\"main-nav\">\
-  <li class=\"nav-logo\"><a href=\"#\"><img src=\"assets/images/brand/LHNameTitle.png\" alt=\"Linde Huang Logo\"></a></li>\
-  <li class=\"nav-link push-right\"><a href=\"#\">Work</a></li>\
-  <li class=\"nav-link\"><a href=\"#\">Writing</a></li>\
-  <li class=\"nav-link\"><a href=\"#\">About</a></li>\
-</ul>";
-
-console.log(navHtml);
-
-document.getElementById("nav-placeholder").innerHTML = navHtml;
