743a4d3efb1574b75da1920908093d737619eea6
Linde Huang <lindeh@Lindes-Work-MacBook.local>
2020-04-29 15:01:52 -0400

    adding Google Analytics tags, cleaning up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,3 @@ window.onload = function() {
   document.getElementById("transitionIn").style.marginTop= "4em";
   document.getElementById("transitionIn").style.fontSize="3.052em";
 };
-
-// document.addEventListener("DOMContentLoaded", function (){
-//   document.getElementById("transitionIn").style.marginTop= "4em";
-//   document.getElementById("transitionIn").style.fontSize="3.052em";
-// }
-// );
