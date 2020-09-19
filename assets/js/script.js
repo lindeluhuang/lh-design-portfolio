febad4fce333cedf9a05ff890cdae10ec033fe09
lindeluhuang <linde.lu.huang@gmail.com>
2020-09-19 09:27:25 -0400

    placed navbar always on top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 window.onload = function() {
-  document.getElementById("transitionIn").style.marginTop= "4em";
-  document.getElementById("transitionIn").style.fontSize="3.052em";
+  // document.getElementById("transitionIn").style.marginTop= "4em";
+  // document.getElementById("transitionIn").style.fontSize="3.052em";
 };
+
