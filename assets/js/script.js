59cf75aa4ebb3a3eaf90d6c076cd40634df32ca9
lindeluhuang <linde.lu.huang@gmail.com>
2020-09-22 09:54:25 -0400

    implementing go to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-window.onload = function() {
-  // document.getElementById("transitionIn").style.marginTop= "4em";
-  // document.getElementById("transitionIn").style.fontSize="3.052em";
-};
+//Get the button:
+mybutton = document.getElementById("goToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
